7ae2facf5648925b1ba5f2038bfd27bd957ad2b7
shushuvo <90605231+shushuvo@users.noreply.github.com>
2021-09-15 14:20:14 -0400

    new2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('welcome fish')
+  res.sendFile('index.html',{root:__dirname})
 })
  
 app.listen(process.env.PORT || 3000)
