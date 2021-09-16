b53c1d1a2dc50058e7e19cdd4ccbc14b0ba76815
shushuvo <90605231+shushuvo@users.noreply.github.com>
2021-09-16 14:29:32 -0400

    12:27 am

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server,{cors:{origin: "*" }})
  
 app.get('/', function (req, res) {
   res.sendFile('index.html',{root:__dirname})
 })
  
-app.listen(process.env.PORT || 3000)
+server.listen(3001, ()=> {
+  console.log("server running...")
+})
+
+io.on('connection', (socket)=>{
+  console.log("User connected:" + socket.id)
+
+  socket.on('message', (data)=>{
+    console.log(data);
+    socket.broadcast.emit('message', data);
+  })
+
+})
