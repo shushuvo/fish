a8522e0885a9b13b064c4b18be599e044a700102
shushuvo <90605231+shushuvo@users.noreply.github.com>
2021-09-16 14:59:39 -0400

    sadsad

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server,{cors:{origin: "*" }})
+const server = require('http').Server(app)
+const io = module.exports.io = require('socket.io')(server)
  
 app.get('/', function (req, res) {
   res.sendFile('index.html',{root:__dirname})
