173ebc584edd33e87274f837d968cd8cc1dab99f
shushuvo <90605231+shushuvo@users.noreply.github.com>
2021-09-19 05:59:54 -0400

    mongoodb

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 
 const server = require('http').createServer(app)
 const io = require('socket.io')(server,{cors:{origin: "*" }})
+
+const mongoose = require("mongoose")
+
+const DB = "mongodb+srv://hutum:_~z4FiRp_nTg6-4@cluster0.4bdzw.mongodb.net/fish?retryWrites=true&w=majority"
+mongoose.connect(DB, function(err, db){console.log('database connected'); 
+let info = db.collection('fish-users');
  
 app.get('/', function (req, res) {
   res.sendFile('index.html',{root:__dirname})
@@ -19,8 +25,19 @@ io.on('connection', (socket)=>{
     console.log(data);
     socket.broadcast.emit('message', data);
   })
+ 
+   socket.on('register', (data)=>{
+    let email = data.email;
+    let name = data.name;
+    let phone = data.phone;
+  
+    info.insert({address:email, name: name, phone:phone });
+
+  })
 
 })
+   });                                    
+                                      
  
 
 
